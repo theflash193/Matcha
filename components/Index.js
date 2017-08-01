030238730dd51e549ebb5fab4d3a977f98637c6b
Gordon RASS-KWASI <grass-kw@e3r1p3.42.fr>
2017-08-01 16:11:20 +0200

    page de login thorique terminer

---
+++ components/Index.js
@@ -0,0 +1,11 @@
+import Index from 'react';
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div className="home">
+          <h1> hello woltd</h1>
+      </div>
+    );
+  }
+}
--- routes/login.js
+++ routes/login.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/matcha');
+// mongoose.connect('mongodb://localhost/matcha');
 /* GET home page. */
-router.post('/', function(req, res, next) {
-  res.render('login');
-  console.log(req.body);
-});
 
 router.get('/', function(req, res, next) {
-  res.render('login');
+  res.render('signin');
 });
 
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/', function(req, res, next) {
+  console.log("post signin");
+  res.redirect('/');
+  console.log("post signin");
+});
 
 module.exports = router;
