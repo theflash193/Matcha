805a3ccbb6ce886265d28386f7bcbce66fb67d28
Gordon RASS-KWASI <grass-kw@e3r1p3.42.fr>
2017-08-01 19:34:17 +0200

    Stylisation en cours

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var login = require('./routes/login');
+var signin = require('./routes/signin');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/signin', login);
+app.use('/signin', signin);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
