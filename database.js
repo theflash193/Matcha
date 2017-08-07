5c31383aab8cf923399860c43f47b42aba309c7b
Gordon RASS-KWASI <grass-kw@e3r1p7.42.fr>
2017-08-07 17:06:35 +0200

    inscription utilisateur + login de l'utilisateur sur le site

--- database.js
+++ database.js
@@ -1,96 +1,56 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-var Db = []
+var url = 'mongodb://localhost:27017/matcha';
 
-var findDocuments = function(db, callback) {
+var findDocuments = function(db, query, callback) {
   // Get the documents collection
-  var collection = db.collection('documents');
+  var collection = db.collection('users');
   // Find some documents
-  collection.find({}).toArray(function(err, docs) {
-    // assert.equal(err, null);
-    // assert.equal(2, docs.length);
+  collection.find(query).toArray(function(err, docs) {
     console.log("Found the following records");
     console.dir(docs);
     callback(docs);
   });
 }
 
-var deleteDocument = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Insert some documents
-  collection.deleteOne({ a : 3 }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Removed the document with the field a equal to 3");
-    callback(result);
-  });
-}
+// var deleteDocument = function(db, query, callback) {
+//   // Get the documents collection
+//   var collection = db.collection('documents');
+//   // Insert some documents
+//   collection.deleteOne(query, function(err, result) {
+//     console.log("Removed the document with the field a equal to 3");
+//     callback(result);
+//   });
+// }
 
 // updateDocument
-var updateDocument = function(db, callback) {
-  // Get the documents collection
-  var collection = db.collection('documents');
-  // Update document where a is 2, set b equal to 1
-  collection.updateOne({ a : 2 }
-    , { $set: { b : 1 } }, function(err, result) {
-    assert.equal(err, null);
-    assert.equal(1, result.result.n);
-    console.log("Updated the document with the field a equal to 2");
-    callback(result);
-  });  
-}
+// var updateDocument = function(db, query, callback) {
+//   var collection = db.collection('documents');
+//   collection.updateOne(query, function(err, result) {
+//     assert.equal(err, null);
+//     assert.equal(1, result.result.n);
+//     console.log("Updated the document with the field a equal to 2");
+//     callback(result);
+//   });  
+// }
 
 // insert
-var insertDocuments = function(db, callback) {
+var insertDocuments = function(db, query, callback) {
   // Get the documents collection
-  var collection = db.collection('documents');
+  var collection = db.collection('users');
   // Insert some documents
-  collection.insertMany([
-    {a : 1}, {a : 2}, {a : 3}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the document collection");
+  collection.insertMany(query, function(err, result) {
+    console.log(query + "is insert in database");
     callback(result);
   });
 }
-
-var url = 'mongodb://localhost:27017/matcha';
-MongoClient.connect(url, function(err, db) {
-	assert.equal(null, err);
-	console.log("Connected correctly to server");
-	deleteDocument(db, function() {
-		db.close();
-	})
-})
-
-
 
 // class APIMongodb {
 
-  constructor(url, DocumentName) {
-	this.url = url;
-	this.DocumentName = DocumentName;
-	MongoClient.connect(this.url, function(err, db) {
-		assert.equal(null, err);
-		console.log("Connected correctly to server");
-		this.db = db
-	})
-  }
-
-const MongodbAPI = new APIMongodb('mongodb://localhost:27017/matcha', 'Matcha');
-// MongodbAPI.createDatabase();
-console.log(MongodbAPI);
-MongodbAPI.createDatabase();
-MongodbAPI.closeDatabase();
-var hello = function() {
-	console.log("hello");
-}
-var i = 3;
 // module.exports = MongodbAPI
 module.exports = {
-	i,
-	hello
+	insertDocuments,
+  findDocuments,
+  MongoClient,
+  url
 }
--- routes/signin.js
+++ routes/signin.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-// var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/matcha';
+var database = require('../database');
+var assert = require('assert');
+
 
 // mongoose.connect('mongodb://localhost/matcha');
 /* GET home page. */
@@ -14,7 +19,39 @@ router.get('/signup', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.redirect('/');
+  database.MongoClient.connect(database.url, function(err, db) {
+    var collection = db.collection('users');
+    collection.findOne({login: req.body.login}, function(err, result) {
+      if (err) throw err
+      if (result) res.redirect('/');
+      else console.log("fail to connect");
+    })
+    // database.findDocuments(db, {login: req.body.login}, function(err, result) {
+    //   console.log(res);
+    //   if (err) console.log('login or password is invalid');
+    //   else res.redirect('/');
+    // })    
+  })
+  // console.log(req);
+});
+
+router.post('/signup', function(req, res, next) {
+  database.MongoClient.connect(database.url, function(err, db) {
+    database.insertDocuments(db, [
+      {
+        login : req.body.password,
+        password : req.body.password,
+        prenom : req.body.prenom,
+        nom : req.body.nom,
+        mail : req.body.mail
+      }],
+      function() {
+        db.close();
+    })
+    console.log('log');
+  })
+  res.redirect('/signin');
+  console.log(req);
 });
 
 module.exports = router;
